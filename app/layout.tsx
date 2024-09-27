d3b6c8b6f6e11a143cf1af8aa799755532c22ce6
roman <roman.pavlenko.job@gmail.com>
2024-09-27 13:57:26 +0300

    added basic global styles. Added header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,20 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import type { Metadata } from "next";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+import { Montserrat, Manrope } from 'next/font/google';
+import { Header } from "./header/Header";
+
+const montserrat = Montserrat({
+  subsets: ['cyrillic'],  
+  variable: '--font-montserrat',
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const manrope = Manrope({
+  subsets: ['cyrillic'],
+  variable: '--font-manrope',
 });
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +28,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${montserrat.variable} ${manrope.variable}`}>
+        <Header />
         {children}
       </body>
     </html>
